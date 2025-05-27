c50a6e73871a6b90990fc4ac475745b8fb273434
vinibarbieri <vinicius190702@hotmail.com>
2025-05-26 21:18:55 -0300

    primeiro deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Welcome to My Website</h1>;
+    return <h1>Júlia, eu amo você mais do que a pipoca azul do lilo. Se você me ama, dá um risadinha!😜</h1>;
 }
 
 export default Home;
